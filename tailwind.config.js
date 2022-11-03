1dc3ded71415dbdcae7976c3155079109a3ab4d5
Matthieu <matthieu@pixel-developpement.com>
2022-11-03 10:35:44 +0100

    + Remove container for xl screen
    + Add white text for news content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,26 @@
 module.exports = {
-	content: [
-		'./layouts/**/*.html.twig',
-		'./themes/**/layouts/**/*.html.twig',
-	],
-	theme: {
-	  colors: {
-		transparent: 'transparent',
-		current: 'currentColor',
-		primary: '#131A25',
-		secondary: '#1F2A3C',
-		color: '#4C6EF2',
-		colorHover: '#4962C2',
-		gris: '#7A8089',
-		white: '#FFFFFF',
-	  }
-	},
+    content: [
+        './layouts/**/*.html.twig',
+        './themes/**/layouts/**/*.html.twig',
+    ],
+    theme: {
+        container: {
+            screens: {
+                'sm': '640px',
+                'md': '768px',
+                'lg': '1024px',
+                'xl': '1280px',
+            }
+        },
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            primary: '#131A25',
+            secondary: '#1F2A3C',
+            color: '#4C6EF2',
+            colorHover: '#4962C2',
+            gris: '#7A8089',
+            white: '#FFFFFF',
+        }
+    },
 }
